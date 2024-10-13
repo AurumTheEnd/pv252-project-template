55df20348133b58288c8db395d680ceb9f8cabad
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2024-10-13 21:16:20 +0200

    task(05): Add new unit/e2e tests

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -1,7 +1,25 @@
-import { expect } from "@playwright/test";
-import { test } from "./coverage_wrapper";
+import { expect } from '@playwright/test';
+import { test } from './coverage_wrapper';
 
-test("find-watman", async ({ page }) => {
-    await page.goto("/");
-    await expect(page.getByAltText("This is watman")).toBeInViewport();
+test('find-watman', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByAltText('This is watman')).toBeInViewport();
+});
+
+test('click-menu-a', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Site A' }).click();
+
+    const child = page.getByRole('link', { name: 'Site A' });
+    const parent = page.getByRole('listitem').filter({ has: child });
+    await expect(parent).toHaveClass(/.*uk-active.*/, { timeout: 10000 });
+});
+
+test('click-div-a', async ({ page }) => {
+    await page.goto('/');
+    await page.getByText('Factorial value 5! is').click();
+
+    const child = page.getByRole('link', { name: 'Site A' });
+    const parent = page.getByRole('listitem').filter({ has: child });
+    await expect(parent).toHaveClass(/.*uk-active.*/, { timeout: 10000 });
 });
